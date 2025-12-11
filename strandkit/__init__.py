a5eae71874cadb7f1d8996be0642d0b9be23c715
lhassa8 <larstray@gmail.com>
2025-12-10 20:42:55 -0500

    Bump version to 2.8.0 for Well-Architected Framework release

--- strandkit/__init__.py
+++ strandkit/__init__.py
@@ -17,7 +17,7 @@ Example usage:
 For more information, see: https://github.com/lhassa8/StrandKit
 """
 
-__version__ = "2.7.0"
+__version__ = "2.8.0"
 __author__ = "StrandKit Contributors"
 
 # Import main agent templates for easy access
